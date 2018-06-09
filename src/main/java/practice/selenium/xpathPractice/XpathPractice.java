7790ddd0162fea350a63230a1406d33a06c0b9a6
a0135561 <elivision.11@gmail.com>
2018-06-09 18:07:42 +0530

    commit in branch1

--- src/main/java/practice/selenium/xpathPractice/XpathPractice.java
+++ src/main/java/practice/selenium/xpathPractice/XpathPractice.java
@@ -5,16 +5,8 @@ public class XpathPractice {
 	public static void main(String[] args) {
 	
 		
-		System.out.println("Push");
-
-		System.out.println("Stash it");
+		System.out.println("Change done on branch 1");
 		
-
-		System.out.println("Second change");
-
-		System.out.println("first change");
-
 		
-		System.out.println("third change");
 	}
 }
