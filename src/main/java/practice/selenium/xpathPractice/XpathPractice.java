eade9197626ba352c04eb55eefbf763764278a93
Dhinesh <a.dhinesh1@gmail.com>
2018-06-29 08:20:53 +0530

    Employee 2 had done some chages

--- src/main/java/practice/selenium/xpathPractice/XpathPractice.java
+++ src/main/java/practice/selenium/xpathPractice/XpathPractice.java
@@ -5,8 +5,7 @@ public class XpathPractice {
 	public static void main(String[] args) {
 	
 
-	System.out.println(" Changes from Branch 1");
-	System.out.println(" Change from Branch 2");
+	System.out.println(" Changes from Employee 2");
 
 	}
 }
